117487c86957a4aff73bc1e99f8d0df8fb1ea66d
Antonio Julián Bueno Fuentes <antoniojulianbueno@gmail.com>
2022-11-07 09:57:24 +0100

    clase con objeto contador v2

--- src/und4/tarea2/act3.php
+++ src/und4/tarea2/act3.php
@@ -10,14 +10,26 @@ class Contador{
     }
 }
 // Llamada a la clase
+echo("Creando contador1");
+echo("<br>");
 $contador1 = new Contador();
+echo("Creando contador2");
+echo("<br>");
 $contador2 = new Contador();
+echo("Creando contador3");
+echo("<br>");
 $contador3 = new Contador();
+echo("Creando contador4");
+echo("<br>");
 $contador4 = new Contador();
+echo("Creando contador5");
+echo("<br>");
 $contador5 = new Contador();
+echo("Creando contador6");
+echo("<br>");
 $contador6 = new Contador();
 echo Contador::getContador();
-
+echo ("<a href='https://github.com/abueno19/Und3_tarea1_DWES/blob/main/src/und4/tarea2/act3.php'>Github</a>");
 
 
 
